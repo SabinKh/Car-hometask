59aa34d88d444cbcfd70b94a2f5b7ac0b04ce9b5
SabinKh <91114252+SabinKh@users.noreply.github.com>
2021-10-13 01:51:13 +0400

    Add files via upload

---
+++ ConsoleApp22/ConsoleApp22/Car.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp22
+{
+    class Car
+    {
+        public double IsledilenBenzin { get; set; } 
+        public double BakTutumu = 150;
+        public double Bakdaolanbenzin { get; set; }
+        public double Kilometr { get; set; }
+        public double QalanBenzin { get; set; }
+        public double Yuzkmyabenzin = 10;
+        
+
+        
+
+        public Car(double km)
+        {
+            Bakdaolanbenzin = 100;
+            Kilometr = km;
+        }
+
+        public void Drive()
+        {
+            double kmyadusenbenzin = Yuzkmyabenzin / 100;
+            if (Bakdaolanbenzin >= (Kilometr * kmyadusenbenzin))
+            {
+                IsledilenBenzin = (Kilometr * kmyadusenbenzin);
+                QalanBenzin = Bakdaolanbenzin - (Kilometr * kmyadusenbenzin);
+
+                Console.WriteLine($"isledilen benzin:{IsledilenBenzin}");
+                Console.WriteLine($"qalan benzin:{QalanBenzin}");
+            }
+            else if(Bakdaolanbenzin < (Kilometr * kmyadusenbenzin))
+            {
+                BenzinDoldur(100);// doldurulacaq benzin
+                Money(500);  //cibinde olan pul
+                
+            }
+            
+
+        }
+
+        public void BenzinDoldur(double doldurulacaqbenzin)
+        {
+            if (doldurulacaqbenzin>BakTutumu)
+            {
+                Console.WriteLine("Benzin dasacaq");
+            }
+            else if (doldurulacaqbenzin<=BakTutumu)
+            {
+                Console.WriteLine("Bak tam dolub,sure bilersiz");
+            }
+        }
+        public void Money(double n)
+        {
+            double qalanpul;
+
+            //1l=2man
+            double benzininqiymet = Kilometr * (Yuzkmyabenzin / 100)*2;
+            if (n<benzininqiymet)
+            {
+                Console.WriteLine("pulun catmir");
+            }
+            else
+            {
+                Console.WriteLine($"Benzin ucun lazim olan pul:{benzininqiymet}");
+                qalanpul = n - benzininqiymet;
+                Console.WriteLine($"Qalanpul: {qalanpul}");
+            }
+
+            
+           
+                   
+            
+            
+        }
+            
+
+         
+    }
+}
---
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car masin = new Car(300);
+            masin.Drive();
+            //masin.Cdrive();
+             
+           
+        }
+    }
+}
